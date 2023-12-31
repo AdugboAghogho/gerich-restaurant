19457157ab4e7993885c2e77a3e0523b9445d3a9
adugbo aghogho <adugboaghogho@gmail.com>
2023-12-31 14:33:05 +0100

    done with header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,9 +9,17 @@ const Header = () => (
     <div className="app__wrapper_info">
       <SubHeading title="Chase The New Flavour" />
       <h1 className="app__header-h1">The Key To Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: '2rem 0' }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Consectetur, sint molestiae. Dolorem saepe iusto fugit 
+        soluta reprehenderit! Voluptatem veritatis voluptatum totam.
+      </p>
+      <button type="button" className="custom__button">Explore Menu</button>
     </div>
 
-    <div className="app__wrapper_img"></div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header img" />
+    </div>
   </div>
 );
 
