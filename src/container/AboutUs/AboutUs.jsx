69259a4a6134ad8158e20fd54bbbc12854109898
adugbo aghogho <adugboaghogho@gmail.com>
2023-12-31 20:19:07 +0100

    added about_knife

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -12,6 +12,18 @@ const AboutUs = () => (
     <div className="app__aboutus-content flex__center">
       <div className="app__aboutus-content_about">
         <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="aboutus_spoon" className='spoon__img' />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Necessitatibus id et nemo inventore voluptatibus libero 
+          cum velit corporis, amet repellat praesentium voluptate 
+          unde, veniam optio dicta deleniti tempore totam natus.
+        </p>
+        <button type="button" className="custom__button">Know More</button>
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="about_knife" />
       </div>
     </div>
   </div>
