a6790f18d7e54ff3ce5d4e2470840780fdf26ef6
adugbo aghogho <adugboaghogho@gmail.com>
2023-12-31 19:49:19 +0100

    added bg

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import './AboutUs.css';
+import { images } from '../../constants';
 
 const AboutUs = () => (
   <div className="app__aboutus app__bg flex__center section__padding" id="about">
-    AboutUs
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="g letter" />
+    </div>
   </div>
 );
 
