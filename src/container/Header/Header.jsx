c7a9509202cfeb63d34a0e7d2d74795cca45b4c4
adugbo aghogho <adugboaghogho@gmail.com>
2023-12-31 13:49:24 +0100

    SubHeading

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
 import { images } from '../../constants'
+import { SubHeading } from '../../components';
 import './Header.css';
 
 const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
-    Header
+    <div className="app__wrapper_info">
+      <SubHeading />
+    </div>
+
+    <div className="app__wrapper_img"></div>
   </div>
 );
 
