773c3150341d0850f7465204c5230cfb93f93520
adugbo aghogho <adugboaghogho@gmail.com>
2023-12-31 13:53:14 +0100

    added Subtitle

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -7,7 +7,8 @@ import './Header.css';
 const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
-      <SubHeading />
+      <SubHeading title="Chase The New Flavour" />
+      <h1 className="app__header-h1">The Key To Fine Dining</h1>
     </div>
 
     <div className="app__wrapper_img"></div>
