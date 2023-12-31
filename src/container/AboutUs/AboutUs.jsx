bb051fbee14aa9e202676c7dc6f4593b90f2e79e
adugbo aghogho <adugboaghogho@gmail.com>
2023-12-31 15:27:28 +0100

    aboutus

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div>
+  <div className="app__aboutus app__bg flex__center section__padding" id="about">
     AboutUs
   </div>
 );
