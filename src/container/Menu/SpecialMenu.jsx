3ae4b12208eabf847107d22111eb0b6a0a17c9e4
adugbo aghogho <adugboaghogho@gmail.com>
2024-01-01 11:55:43 +0100

    special menu

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -16,7 +16,7 @@ const SpecialMenu = () => (
         <p className="app__specialsMenu_menu_heading">Wine & Beer</p>
         <div className="app__specialsMenu_menu_items">
           {data.wines.map((wine, index) => (
-            <p>{wine.title}</p>
+            <MenuItem key={wine.title + index} title={wine.price} price={wine.price} tags={wine.tags} />
           ))}
         </div>
       </div>
@@ -29,7 +29,7 @@ const SpecialMenu = () => (
         <p className="app__specialsMenu_menu_heading">Cocktails</p>
         <div className="app__specialsMenu_menu_items">
           {data.cocktails.map((cocktail, index) => (
-            <p>{cocktail.title}</p>
+            <MenuItem key={cocktail.title + index} title={cocktail.title} price={cocktail.price} tags={cocktail.tags} />
           ))}
         </div>
       </div>
