16083458d362e966e27aff3f9ba199d5e5566522
adugbo aghogho <adugboaghogho@gmail.com>
2024-01-03 11:39:39 +0100

    sth fixed

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -6,7 +6,7 @@ import { SubHeading } from '../../components';
 
 const Chef = () => (
   <div className="app__bg app__wrapper section__padding">
-    <div className="app__wrapper_img app__wrapper_img-reverse">
+    <div className="app__wrapper_img wrapper_img-reverse">
       <img src={images.chef} alt="chef" />
     </div>
 
