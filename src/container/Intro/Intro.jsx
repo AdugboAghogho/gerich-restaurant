4a7ae77ded8a634a2f473887db0120c354a778c7
adugbo aghogho <adugboaghogho@gmail.com>
2024-01-03 12:05:08 +0100

    startinf intro

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
-
+ 
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
 import './Intro.css';
+import { meal } from '../../constants';
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+
+  return (
+    <div className="app__video">
+      <video
+        src={meal}
+        ref={vidRef}
+        type="video/mp4"
+        loop
+        controls={false}
+      />
+    </div>
+  )
+};
 
 export default Intro;
