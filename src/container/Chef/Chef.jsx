b9db7b03275ceb061304a7b400ea5b74020aa45b
adugbo aghogho <adugboaghogho@gmail.com>
2024-01-03 11:33:09 +0100

    fixed a typo

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -7,7 +7,7 @@ import { SubHeading } from '../../components';
 const Chef = () => (
   <div className="app__bg app__wrapper section__padding">
     <div className="app__wrapper_img app__wrapper_img-reverse">
-      <img src={images.chef} alt=""chef />
+      <img src={images.chef} alt="chef" />
     </div>
 
     <div className="app__wrapper_info">
