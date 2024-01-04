497f2a05a29194cfa37b47f047661dfaeaabb7a3
adugbo aghogho <adugboaghogho@gmail.com>
2024-01-04 16:49:44 +0100

    app__gallery-images_arrow

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -7,10 +7,30 @@ import { images, data } from '../../constants';
 import './Gallery.css';
 
 const Gallery = () => {
+  const scrollRef = React.useRef(null);
   
   return (
-    <div>
-      Gallery
+    <div className="app__gallery flex__center">
+      <div className="app__gallery-content">
+        <SubHeading title="Instagram" />
+        <h1 className="headtext__cormorant">Photo Gallery</h1>
+        <p className="p__opensans" style={{ color: '#AAA', marginTop: '2rem' }}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, 
+          sit laboriosam cupiditate nulla voluptatibus autem.
+        </p>
+
+        <button type="button" className="custom__button">View More</button>
+      </div>
+
+      <div className="app__gallery-images">
+        <div className="app__gallery-images_container" ref={scrollRef}>
+          .
+        </div>
+
+        <div className="app__gallery-images_arrow">
+          <BsArrowLeftShort className="gallery__arrow-icons" onClick={() => scrollRef('left')} />
+        </div>
+      </div>
     </div>
   )
 };
