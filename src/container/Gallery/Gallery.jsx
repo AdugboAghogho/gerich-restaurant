b4433219c1ebe710f6bf59ba1bd51a8c8bc87b37
adugbo aghogho <adugboaghogho@gmail.com>
2024-01-04 16:35:30 +0100

    added bd

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
 
 import { SubHeading } from '../../components';
 import { images, data } from '../../constants';
 
 import './Gallery.css';
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
-
+const Gallery = () => {
+  
+  return (
+    <div>
+      Gallery
+    </div>
+  )
+};
 export default Gallery;
