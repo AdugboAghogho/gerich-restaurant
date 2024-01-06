0f22684c1421bfb973155a5b009d4a1b586f399d
adugbo aghogho <adugboaghogho@gmail.com>
2024-01-06 14:27:07 +0100

    fixed typos

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -44,7 +44,7 @@ const Gallery = () => {
           ))}
         </div>
 
-        <div className="app__gallery-images_arrow">
+        <div className="app__gallery-images_arrows">
           <BsArrowLeftShort className="gallery__arrow-icons" onClick={() => scrollRef('left')} />
           <BsArrowRightShort className="gallery__arrow-icons" onClick={() => scrollRef('right')} />
         </div>
