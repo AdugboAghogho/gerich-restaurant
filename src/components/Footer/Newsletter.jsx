daddbc5f6365dc12ae8a2777d47a7bb27f11f341
adugbo aghogho <adugboaghogho@gmail.com>
2024-01-07 16:04:27 +0100

    trying to fix y it isn't not showing

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
 import './Newsletter.css';
+import SubHeading from './../SubHeading/SubHeading';
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className="app__newsletter">
+    <div className="app__newsletter-heading">
+      <SubHeading title="Newsletter" />
+      <h1 className="headtext__cormorant">Subscribe To Our Newsletter</h1>
+      <p className="p__opensans">And Never Miss Latest Updates.</p>
+    </div>
+
+    <div className="app__newsletter-input flex__center">
+      <input 
+        type='email'
+        placeholder="Enter your E-Mail Address "
+      />
+        <button type="button" className="custom__button">Subscribe</button>
+    </div>
   </div>
 );
 
